5f9e520d66dba69f15a13e7485bfcce3ac5fdcac
fernandodeleoncorrea@gmail.com <fernandodeleoncorrea@gmail.com>
2020-10-10 00:13:55 -0300

    Snackbar

--- app/src/main/java/com/example/pentagrama/MainActivity.java
+++ app/src/main/java/com/example/pentagrama/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.pentagrama;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.sip.SipSession;
 import android.os.Bundle;
 import android.view.View;
+import android.util.Log;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +25,16 @@ public class MainActivity extends AppCompatActivity {
         miFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //Toast.makeText(getBaseContext(), getResources().getString(R.string.mensaje), Toast.LENGTH_SHORT).show();
+                Snackbar.make(v, getResources().getString(R.string.mensaje),Snackbar.LENGTH_LONG)
+                        .setAction(getResources().getString(R.string.texto_accion), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Log.i("SNACKBAR", "presionado");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.colorPrimary))
+                        .show();
             }
         });
     }
