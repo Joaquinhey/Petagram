4b2f926962c21187bce5a8f20c5cfaed68654dcc
fernandodeleoncorrea@gmail.com <fernandodeleoncorrea@gmail.com>
2020-10-15 08:46:11 -0300

    RefreshIndicator

--- app/src/main/java/com/example/pentagrama/MainActivity.java
+++ app/src/main/java/com/example/pentagrama/MainActivity.java
@@ -7,19 +7,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.util.Log;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    SwipeRefreshLayout sfiMiIndicadorRefresh;
+    ListView lstMiLista;
+    Adapter adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         agregarFAB();
+        sfiMiIndicadorRefresh = (SwipeRefreshLayout) findViewById(R.id.sfiMiIndicadorRefresh);
+        lstMiLista = (ListView) findViewById(R.id.lstMiLista);
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, planetas));
+
+        sfiMiIndicadorRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refrescandoContenido();
+            }
+        });
     }
+
+    public void refrescandoContenido(){
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, planetas));
+        sfiMiIndicadorRefresh.setRefreshing(false);
+    }
+
     public void agregarFAB(){
         FloatingActionButton miFAB = (FloatingActionButton) findViewById(R.id.fabMiFAB);
         miFAB.setOnClickListener(new View.OnClickListener() {
